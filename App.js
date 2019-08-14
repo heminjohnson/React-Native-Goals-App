686c6e3a0bfefbb9a387a0ec05e4bd31cb5809e2
Hemin <achayan1992@gmail.com>
2019-08-14 18:06:50 +0200

    feat: add input fields and buttons

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={{
+      padding: 50
+    }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <TextInput placeholder={'Course Goal'}
+                   style={{
+                     width: '80%',
+                     borderColor: 'black',
+                     borderWidth: 1,
+                     padding: 10
+                   }}
+        />
+        <Button title={'ADD'}/>
+      </View>
+      <View>
+
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
