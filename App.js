8825094b4b7d65870df5830b04f79512a73e1137
Hemin <achayan1992@gmail.com>
2019-08-14 19:05:09 +0200

    feat: add list items that outputs on clicking the button

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput placeholder={'Course Goal'}
-                   style={styles.input}/>
-        <Button title={'ADD'}/>
+                   style={styles.input}
+                   onChangeText={goalInputHandler}
+                   value={enteredGoal}
+        />
+        <Button
+          title={'ADD'}
+          onPress={addGoalHandler}
+        />
       </View>
       <View>
-
+        {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
       </View>
-      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -31,5 +47,12 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
